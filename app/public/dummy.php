c6a7509b6a6a500b025313329b3a50dabbd3a7ea
sneharaokelley <54503035+sneharaokelley@users.noreply.github.com>
2019-09-18 08:45:14 -0400

    First commit

---
+++ app/public/dummy.php
@@ -0,0 +1,30 @@
+[
+
+{
+  "patientGuid":"P0001",
+  "firstName":"Sneha",
+  "lastName":"Rao",
+  "dob":"1995-10-12",
+  "sexAtBirth":"F",
+  "priority":"high"
+},
+
+{
+  "patientGuid":"P0002",
+  "firstName":"Shruti",
+  "lastName":"Iyer",
+  "dob":"2012-09-01",
+  "sexAtBirth":"F",
+  "priority":"high"
+},
+
+{
+  "patientGuid":"P0003",
+  "firstName":"Jay",
+  "lastName":"Sheladiya",
+  "dob":"1994-09-01",
+  "sexAtBirth":"M",
+  "priority":"high"
+}
+
+]
---
+++ app/public/js/waiting.app.js
@@ -0,0 +1,23 @@
+var waitingApp = new Vue({
+  el: '#patientWaitingApp',
+  data:{
+
+    patients:[
+
+    ]
+  },
+  methods:{
+    fetchPatients(){
+      fetch('dummy.php')
+      .then(response => response.json())
+      .then(json => waitingApp.patients=json);
+
+    }
+  },
+
+  created (){
+    this.fetchPatients();
+  }
+}
+
+);
